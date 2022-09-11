870c0c6ed64213530153bd9091124fb08f41d887
changethecode <jim-frey@web.de>
2022-09-11 13:52:53 +0200

    add init logic project and test project

---
+++ Nuspec.CompleteChecker/Nuspec.CompleteChecker.Test/NuspecDependencyLoaderTests.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using Nuspec.CompleteChecker.Model;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace Nuspec.CompleteChecker.Test
+{
+    public class NuspecDependencyLoaderTest
+    {
+        [Fact]
+        public void LoadDependenciesOfNuspec_Happy()
+        {
+            var loader = new NuspecDependencyLoader();
+
+            var expectedPackages = new List<Package>
+            {
+                new Package("Newtonsoft.Json","11.0.2"),
+                new Package("RestSharp", "106.3.1"),
+                new Package("Selenium.Support", "3.14.0"),
+                new Package("Selenium.WebDriver", "3.14.0"),
+            };
+
+            var allNuspecDependencies = loader.LoadAllPackagesDependencies(Path.Combine(Directory.GetCurrentDirectory(), "TestFiles\\QuickNuspecExample.nuspec"));
+
+            allNuspecDependencies.Should().HaveCount(4);
+            allNuspecDependencies.Should().BeEquivalentTo(expectedPackages);
+        }
+    }
+}
---
+++ Nuspec.CompleteChecker/Nuspec.CompleteChecker/CsprojDependencyLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nuspec.CompleteChecker.Model;
+using U8Xml;
+
+namespace Nuspec.CompleteChecker
+{
+    internal class CsprojDependencyLoader
+    {
+        public static IReadOnlyCollection<string> DirSearch(string rootDirectory)
+        {
+            var allfiles = new List<string>();
+            foreach (string d in Directory.GetDirectories(rootDirectory))
+            {
+                foreach (string f in Directory.GetFiles(d, "*.csproj").Where(c => !c.Contains("Test")))
+                {
+                    allfiles.Add(f);
+                }
+                DirSearch(d);
+            }
+
+            return allfiles;
+        }
+
+        public IReadOnlyCollection<Package> LoadAllPackagesDependencies(string filePath)
+        {
+            var allCsProjPackageReferences = new List<Package>();
+
+            using (XmlObject xml = XmlParser.ParseFile(filePath))
+            {
+                XmlNode root = xml.Root;
+
+                var packageReferences = root.Descendants.Where(c => c.Name == "PackageReference").Select(c =>
+                new Package(
+                    c.Attributes.FirstOrDefault(element => element.Name == "Include").Value.Value.ToString().Trim(),
+                    c.Attributes.FirstOrDefault(element => element.Name == "Version").Value.Value.ToString().Trim()))
+                .ToArray();
+
+                allCsProjPackageReferences.AddRange(packageReferences);
+            }
+            return allCsProjPackageReferences;
+        }
+    }
+}
