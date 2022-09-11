e100f62d300bd1936a90e80ad4161964d93e77e8
changethecode <jim-frey@web.de>
2022-09-11 13:57:06 +0200

    add more simple tests

--- Nuspec.CompleteChecker/Nuspec.CompleteChecker.Test/NuspecDependencyLoaderTests.cs
+++ Nuspec.CompleteChecker/Nuspec.CompleteChecker.Test/NuspecDependencyLoaderTests.cs
@@ -26,5 +26,45 @@ namespace Nuspec.CompleteChecker.Test
             allNuspecDependencies.Should().HaveCount(4);
             allNuspecDependencies.Should().BeEquivalentTo(expectedPackages);
         }
+
+
+        [Fact]
+        public void LoadDependenciesOfNuspec_WrongDependenciesExpected_ShouldFail()
+        {
+            var loader = new NuspecDependencyLoader();
+
+            var expectedPackages = new List<Package>
+            {
+                new Package("Newtonsoft.Json","11.0.2"),
+                new Package("RestSharp", "106.3.1"),
+                new Package("Selenium.Support", "3.14.0"),
+                new Package("Selenium.WebDriver", "3.14.0"),
+                new Package("Selenium.Web", "3.14.0"),
+            };
+
+            var allNuspecDependencies = loader.LoadAllPackagesDependencies(Path.Combine(Directory.GetCurrentDirectory(), "TestFiles\\QuickNuspecExample.nuspec"));
+
+            allNuspecDependencies.Should().HaveCount(4);
+            allNuspecDependencies.Should().NotBeEquivalentTo(expectedPackages);
+        }
+
+        [Fact]
+        public void LoadDependenciesOfNuspec_WrongVersionExpected_ShouldFail()
+        {
+            var loader = new NuspecDependencyLoader();
+
+            var expectedPackages = new List<Package>
+            {
+                new Package("Newtonsoft.Json","11.0.2"),
+                new Package("RestSharp", "106.3.1"),
+                new Package("Selenium.Support", "3.14.0"),
+                new Package("Selenium.WebDriver", "3.19.0"),
+            };
+
+            var allNuspecDependencies = loader.LoadAllPackagesDependencies(Path.Combine(Directory.GetCurrentDirectory(), "TestFiles\\QuickNuspecExample.nuspec"));
+
+            allNuspecDependencies.Should().HaveCount(4);
+            allNuspecDependencies.Should().NotBeEquivalentTo(expectedPackages);
+        }
     }
 }
